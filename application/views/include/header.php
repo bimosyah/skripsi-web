a061e66311cbde47058ae4c22dc4979d05240756
bimosyah <bimosyahputro88@gmail.com>
2020-03-30 22:10:36 +0700

    ganti template

---
+++ application/views/include/header.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Dashboard V.1 | Kiaalap - Kiaalap Admin Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+      ============================================ -->
+      <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('assets/img/favicon.ico') ?>">
+    <!-- Google Fonts
+      ============================================ -->
+      <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <!-- Bootstrap CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css') ?>">
+    <!-- owl.carousel CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.css') ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.theme.css') ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.transitions.css') ?>">
+    <!-- animate CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/animate.css') ?>">
+    <!-- normalize CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/normalize.css') ?>">
+    <!-- meanmenu icon CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/meanmenu.min.css') ?>">
+    <!-- main CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?>">
+    <!-- educate icon CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/educate-custon-icon.css') ?>">
+    <!-- morrisjs CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/morrisjs/morris.css') ?>">
+    <!-- mCustomScrollbar CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/scrollbar/jquery.mCustomScrollbar.min.css') ?>">
+    <!-- metisMenu CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/metisMenu/metisMenu.min.css') ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/metisMenu/metisMenu-vertical.css') ?>">
+    <!-- calendar CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/calendar/fullcalendar.min.css') ?>">
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/calendar/fullcalendar.print.min.css') ?>">
+    <!-- style CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+    <!-- responsive CSS
+      ============================================ -->
+      <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css') ?>">
+    <!-- modernizr JS
+      ============================================ -->
+      <script src="<?php echo base_url('assets/js/vendor/modernizr-2.8.3.min.js') ?>"></script>
+    </head>
+
+    <body>
+    <!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+  <!-- Start Left menu area -->
+  <div class="left-sidebar-pro">
+    <nav id="sidebar" class="">
+      <div class="sidebar-header">
+        <a href="index.html"><img class="main-logo" src="<?php echo base_url('assets/img/logo/logo.p') ?>ng" alt="" /></a>
+        <strong><a href="index.html"><img src="<?php echo base_url('assets/img/logo/logosn.p') ?>ng" alt="" /></a></strong>
+      </div>
+      <div class="left-custom-menu-adp-wrap comment-scrollbar">
+        <nav class="sidebar-nav left-sidebar-menu-pro">
+          <ul class="metismenu" id="menu1">
+          <li>
+            <a href=""><span class="educate-icon educate-home icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Dashboard</span></a>
+          </li>
+          <!-- <li>
+            <a href=""><span class="sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Event</span></a>
+          </li>
+          <li>
+            <a class="has-arrow" aria-expanded="false"><span class="icon-wrap"></span> <span class="mini-click-non">Professors</span></a>
+            <ul class="submenu-angle" aria-expanded="false">
+              <li><a title="All Professors" href="all-professors.html"><span class="mini-sub-pro">All Professors</span></a></li>
+              <li><a title="Add Professor" href="add-professor.html"><span class="mini-sub-pro">Add Professor</span></a></li>
+              <li><a title="Edit Professor" href="edit-professor.html"><span class="mini-sub-pro">Edit Professor</span></a></li>
+              <li><a title="Professor Profile" href="professor-profile.html"><span class="mini-sub-pro">Professor Profile</span></a></li>
+            </ul>
+          </li>       -->   
+        </ul>
+      </nav>
+    </div>
+  </nav>
+</div>
+<!-- End Left menu area -->
+<!-- Start Welcome area -->
+<div class="all-content-wrapper">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="logo-pro">
+          <a href="index.html"><img class="main-logo" src="<?php echo base_url('assets/img/logo/logo.p') ?>ng" alt="" /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="header-advance-area">
+    <div class="header-top-area">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="header-top-wraper">
+              <div class="row">
+                <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
+                  <div class="menu-switcher-pro">
+                    <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+                     <i class="educate-icon educate-nav"></i>
+                   </button>
+                 </div>
+               </div>
+               <div class="col-lg-6 col-md-7 col-sm-6 col-xs-12">
+                <div class="header-top-menu tabl-d-n">
+                  <ul class="nav navbar-nav mai-top-nav">
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                <div class="header-right-info">
+                  <ul class="nav navbar-nav mai-top-nav header-right-menu">
+                    <li class="nav-item">
+                      <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
+                       <img src="<?php echo base_url('assets/img/product/pro4.j') ?>pg" alt="" />
+                       <span class="admin-name">Prof.Anderson</span>
+                       <i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
+                     </a>
+                     <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                      <li><a href="#"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a>
+                      </li>
+                    </ul>
+                  </li>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+</div>
+</div>
+</div>
+<!-- Mobile Menu start -->
+<div class="mobile-menu-area">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="mobile-menu">
+          <nav id="dropdown">
+            <ul class="mobile-menu-nav">
+              <li><a href="">Dashboard</a></li>
+              <!-- <li><a href="">Event</a></li>
+              <li><a data-toggle="collapse" data-target="#demoevent" href="#">Professors <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
+                <ul id="demoevent" class="collapse dropdown-header-top">
+                  <li><a href="all-professors.html">All Professors</a>
+                  </li>
+                  <li><a href="add-professor.html">Add Professor</a>
+                  </li>
+                  <li><a href="edit-professor.html">Edit Professor</a>
+                  </li>
+                  <li><a href="professor-profile.html">Professor Profile</a>
+                  </li>
+                </ul>
+              </li> -->
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
